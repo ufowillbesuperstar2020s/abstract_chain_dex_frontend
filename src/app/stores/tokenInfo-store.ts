c6de928c902bbf78224c1b469ebf089cb62c3d0b
ufowillbesuperstar2020s <tuannlee992@gmail.com>
2025-11-27 23:34:50 +0700

    comment

--- src/app/stores/tokenInfo-store.ts
+++ src/app/stores/tokenInfo-store.ts
@@ -5,6 +5,7 @@ import type { TokenMetadata } from '@/types/api';
 import { fetchTokenMetadataFromApi } from '@/app/actions/token';
 
 type TokenInfoState = {
+  // co
   tokenAddress: string | null;
   tokenMetadata: TokenMetadata | null;
   isLoading: boolean;
